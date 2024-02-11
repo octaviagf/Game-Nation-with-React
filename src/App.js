f1c5290187b85d488ae88a5a8e65454e1b9bc603
octaviagf <oagf0616@gmail.com>
2024-02-11 19:33:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Navbar />
+      <Header />
+      <Games />
+      <Footer />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <img src="img/index.jpg" alt="Game Nation" />
+      </nav>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <img src="img/header.png" alt="Game On" />
       </header>
     </div>
   );
 }
 
-export default App;
+function Games() {
+  return (
+    <main>
+      <h1>Games</h1>
+      <Game
+        photoName="img/call of duty.png"
+        info="Representa situaciones de combate y guerra desde perspectivas históricas o contemporáneas."
+        name="Call Of Duty"
+      />
+      <Game
+        photoName="img/fortnite.jpg"
+        info="Se centra principalmente en la supervivencia en un mundo postapocalíptico."
+        name="Fortnite"
+      />
+      <Game
+        photoName="img/league.jpg"
+        info="Gira en torno a la competencia estratégica entre dos equipos de jugadores."
+        name="League Of Legends"
+      />
+      <Game
+        photoName="img/minecraft.png"
+        info="Se desarrolla en un vasto mundo tridimensional compuesto por bosques, montañas, desiertos, océanos, cuevas."
+        name="Minecraft"
+      />
+    </main>
+  );
+}
+
+function Game({ name, info, photoName }) {
+  const [color, setColor] = useState("black");
+
+  function changeColor() {
+    const newColor = color === "black" ? "blue" : "black";
+    setColor(newColor);
+  }
+
+  return (
+    <div className="cards" style={{ backgroundColor: color }}>
+      <img src={photoName} alt={name} />
+      <div>
+        <p>{info}</p>
+        <button onClick={() => changeColor()}>Comprar</button>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <span>© 2024 Game Nation</span>
+      </footer>
+    </div>
+  );
+}
